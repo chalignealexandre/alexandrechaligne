edec89c352f039635ce62b678b15f74c327e3307
lesrecoltantsbordeaux <contact@lesrecoltants.fr>
2025-12-06 10:18:01 +0100

    Fix: Replace terser with esbuild for build and add type=module to script

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       input: {
         main: './index.html',
